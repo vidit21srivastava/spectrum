f9784a1e3ce0ba42744b9ebd5b7eab5294161318
vidit21srivastava <vidit21srivastava@gmail.com>
2026-01-13 09:49:27 +0530

    Added Handlebars support for dynamic endpoint and body resolution in HTTP request executor; Updated variableName and method handling

--- src/app/features/executions/components/http-request/executor.ts
+++ src/app/features/executions/components/http-request/executor.ts
@@ -1,11 +1,14 @@
 import type { NodeExecutor } from "@/app/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+
+Handlebars.registerHelper("JSON", (context) => JSON.stringify(context, null, 2));
 
 type httpRequestData = {
-    variableName?: string;
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string;
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
 };
 
@@ -29,15 +32,23 @@ export const httpRequestExecutor: NodeExecutor<httpRequestData> = async (
         throw new NonRetriableError("HTTP Request Node: Variable name not configured");
     }
 
+    if (!data.method) {
+        // TODO : PUblish "error" state for http request
+        throw new NonRetriableError("HTTP Request Node: Method not configured");
+    }
     // const result = await step.run("http-request", async () => context);
     // const result = await step.fetch(data.endpoint);
     const result = await step.run("http-request", async () => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+
+        const endpoint = Handlebars.compile(data.endpoint)(context); // https://.../{{todo.httpResponse.data.userID}}
+        // console.log("ENDPOINT", { endpoint });
+        const method = data.method;
 
         const options: KyOptions = { method };
         if (["POST", "PUT", "PATCH"].includes(method)) {
-
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            console.log("Body:", { resolved });
+            JSON.parse(resolved); //--> to protect from any invalid JSON being passed
             options.body = data.body;
             options.headers = {
                 "Content-Type": "application/json",
@@ -57,18 +68,11 @@ export const httpRequestExecutor: NodeExecutor<httpRequestData> = async (
             },
         };
 
-        if (data.variableName) {
-            return {
-                ...context,
-                [data.variableName]: responsePayload,
-            };
-        }
 
         return {
             ...context,
-            ...responsePayload,
+            [data.variableName]: responsePayload,
         };
-
 
     });
 
