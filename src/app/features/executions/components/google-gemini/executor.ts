d669bce426caee82d1ffac1aef967627a6227af3
vidit21srivastava <vidit21srivastava@gmail.com>
2026-01-25 01:43:41 +0530

    Added defensive check for text type in googleGeminiExecutor

--- src/app/features/executions/components/google-gemini/executor.ts
+++ src/app/features/executions/components/google-gemini/executor.ts
@@ -82,7 +82,7 @@ export const googleGeminiExecutor: NodeExecutor<googleGeminiData> = async (
             },
         );
 
-        const text = steps[0].content[0].type === "text"
+        const text = steps?.[0]?.content?.[0]?.type === "text" //defensive check
             ? steps[0].content[0].text
             : "";
 
